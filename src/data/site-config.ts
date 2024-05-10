a15227a894fd5f6fd88ac9bb4ee5321b68186c59
Sarah Wilkes <aremnius@gmail.com>
2024-05-10 14:59:41 -0400

    more cleanup & begin banner img placement

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -9,13 +9,6 @@ export type Link = {
     href: string;
 };
 
-// export type Hero = {
-//     title?: string;
-//     text?: string;
-//     image?: Image;
-//     actions?: Link[];
-// };
-
 export type SiteConfig = {
     logo?: Image;
     title: string;
@@ -25,7 +18,6 @@ export type SiteConfig = {
     headerNavLinks?: Link[];
     footerNavLinks?: Link[];
     socialLinks?: Link[];
-    // hero?: Hero;
     postsPerPage?: number;
 };
 
@@ -43,7 +35,11 @@ const siteConfig: SiteConfig = {
             href: '/'
         },
         {
-            text: 'Blog',
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Blog Archive',
             href: '/blog'
         },
         {
@@ -51,32 +47,13 @@ const siteConfig: SiteConfig = {
             href: '/tags'
         }
     ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        }
-    ],
+    footerNavLinks: [],
     socialLinks: [
         {
             text: 'Flickr',
             href: 'https://www.flickr.com/photos/thirdlayer/'
         }
     ],
-    // hero: {
-    //     title: 'Hi There & Welcome to My Corner of the Web!',
-    //     text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-    //     image: {
-    //         src: '/hero.jpeg',
-    //         alt: 'A person sitting at a desk in front of a computer'
-    //     },
-    //     actions: [
-    //         {
-    //             text: 'Get in Touch',
-    //             href: '/contact'
-    //         }
-    //     ]
-    // },
 
     postsPerPage: 8
 };
